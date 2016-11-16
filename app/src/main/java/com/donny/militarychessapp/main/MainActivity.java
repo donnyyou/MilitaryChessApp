3e5be8711d8e3364777d72f75123a0d62e63a8c5
youansheng <dice@pku.edu.cn>
2016-11-16 16:14:59 +0800

    Refactor the code & add bluetooth match function.

--- app/src/main/java/com/donny/militarychessapp/main/MainActivity.java
+++ app/src/main/java/com/donny/militarychessapp/main/MainActivity.java
@@ -1,7 +1,9 @@
-package com.donny.militarychessapp;
+package com.donny.militarychessapp.main;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.donny.militarychessapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
