d0f19f9df1ce3fd3d699d1ee0d567e326e33b327
youansheng <dice@pku.edu.cn>
2016-11-22 08:21:51 +0800

    Context of the app.

---
+++ app/src/main/java/com/donny/militarychessapp/context/ContextUtil.java
@@ -0,0 +1,21 @@
+package com.donny.militarychessapp.context;
+
+/**
+ * Created by youansheng on 2016/5/31.
+ */
+import android.app.Application;
+
+public class ContextUtil extends Application {
+    private static ContextUtil instance;
+
+    public static ContextUtil getInstance() {
+        return instance;
+    }
+
+    @Override
+    public void onCreate() {
+        // TODO Auto-generated method stub
+        super.onCreate();
+        instance = this;
+    }
+}
