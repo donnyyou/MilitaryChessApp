5b611723dd8a3e2995503083003a1e34291aefdc
youansheng <dice@pku.edu.cn>
2016-11-22 08:20:48 +0800

    Use imageview represent every chess.

--- app/src/main/java/com/donny/militarychessapp/var/Variable.java
+++ app/src/main/java/com/donny/militarychessapp/var/Variable.java
@@ -1,17 +1,18 @@
 package com.donny.militarychessapp.var;
 
+import android.graphics.drawable.Drawable;
+import android.widget.ImageView;
+
 import com.donny.militarychessapp.chess.Position;
+import com.donny.militarychessapp.context.ContextUtil;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 
-import javax.swing.Icon;
-import javax.swing.JLabel;
-
 public class Variable {
     public static HashMap<String, String> redKeyToImage = new HashMap<String, String>();
     public static HashMap<String, String> blueKeyToImage = new HashMap<String, String>();
-    public static JLabel[] ItemBox = new JLabel[61];//��¼60��picturebox����������
+    public static ImageView[] ItemBox = new ImageView[61];
     public static Position[] ChessPos = new Position[61];
     public static boolean isStart = false;
     public static boolean isClicked = false;
@@ -23,12 +24,12 @@ public class Variable {
     public static boolean isEnd = false;
     public static int searchDeepth = 1;
     public static boolean forDFS = false;
-    public static Icon iconIndex1;
-    public static Icon iconIndex2;
+    public static Drawable iconIndex1;
+    public static Drawable iconIndex2;
     public static int threadNum = 0;
     public static boolean flag = false;
     public static int threadAnimate = 0;
     public static int temp = 0;
     public static ArrayList<Position> posList = new ArrayList<Position>();
-    public static JLabel picbox = new JLabel();
+    public static ImageView picbox = new ImageView(ContextUtil.getInstance());
 }
